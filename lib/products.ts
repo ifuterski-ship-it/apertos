6174e7d8d7b042772205a757d30c64f0335096fc
ifuterski <ifuterski@icloud.com>
2026-09-19 14:33:35 +0100

    sakura dragon: back image first in carousel and primary

--- lib/products.ts
+++ lib/products.ts
@@ -209,10 +209,10 @@ export const products: Product[] = [
     name: "Sakura Dragon",
     price: 50,
     priceLabel: "£50.00",
-    image: "/products/sakura-dragon-front.jpeg",
+    image: "/products/sakura-dragon-back.jpeg",
     images: [
-      "/products/sakura-dragon-front.jpeg",
       "/products/sakura-dragon-back.jpeg",
+      "/products/sakura-dragon-front.jpeg",
       "/products/sakura-dragon-detail-1.jpeg",
       "/products/sakura-dragon-detail-2.jpeg"
     ],
