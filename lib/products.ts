37824284b565499d75e7446c35fe8614b1855a3a
ifuterski <ifuterski@icloud.com>
2026-09-19 14:40:07 +0100

    sakura dragon set: reorder carousel (detail-2, detail-1, front, back)

--- lib/products.ts
+++ lib/products.ts
@@ -209,12 +209,12 @@ export const products: Product[] = [
     name: "Sakura Dragon",
     price: 50,
     priceLabel: "£50.00",
-    image: "/products/sakura-dragon-front.jpeg",
+    image: "/products/sakura-dragon-detail-2.jpeg",
     images: [
-      "/products/sakura-dragon-front.jpeg",
-      "/products/sakura-dragon-back.jpeg",
+      "/products/sakura-dragon-detail-2.jpeg",
       "/products/sakura-dragon-detail-1.jpeg",
-      "/products/sakura-dragon-detail-2.jpeg"
+      "/products/sakura-dragon-front.jpeg",
+      "/products/sakura-dragon-back.jpeg"
     ],
     sizes: ["XS", "S", "M", "L", "XL"],
     category: "Bundle",
