928b488d43c75fc78ebbcb6c0fddc1f8ce5a4529
ifuterski <ifuterski@icloud.com>
2026-09-26 16:15:26 +0100

    Remove last photo from adult Essential Hoodie

--- lib/products.ts
+++ lib/products.ts
@@ -151,8 +151,7 @@ export const products: Product[] = [
     images: [
       "/products/hoodie-grey-front.jpeg",
       "/products/hoodie-grey-hover.jpeg",
-      "/products/hoodie-grey-hover-2.jpeg",
-      "/products/hoodie-grey-back.png.jpeg"
+      "/products/hoodie-grey-hover-2.jpeg"
     ],
     sizes: ["S", "M", "L", "XL", "2XL"],
     colours: ["Grey"],
